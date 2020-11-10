68424f442ffb3ad88b7b1d638d63b4569cf3ec10
cquinn85 <cequinn300@gmail.com>
2020-11-10 13:56:52 -0600

    bank encapsulation complete

---
+++ BankEncapsulation2/BankAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation2
+{
+    public class BankAccount
+    {
+        public BankAccount()//constructor: special member method
+        {
+        }
+        // create a A private field of type double named balance 
+        //with a value of 0
+        private double balance = 0;
+
+        //Define a method named Deposit that will accept 
+        //a double and store that value in the balance field
+        public void Deposit(double amount) //this is the encapsulation
+        {
+            balance = amount;
+        }
+
+        //Define a method named GetBalance that will 
+        //return the amount stored in the balance field 
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ BankEncapsulation2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BankEncapsulation2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //In the main method of your application, 
+            //create a new instance of the BankAccount class. 
+           
+
+            var account = new BankAccount();// use BAnkAccount in place of var
+                                            // after new you call the constructor
+
+            //Allow the user of the application to Deposit money 
+            //and retrieve their balance through the console.
+            Console.WriteLine("How much would you like to deposit:");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            //call the deposit method
+            account.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you. Your balance is now {account.GetBalance()}");
+
+        }
+    }
+}
